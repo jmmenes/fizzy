3ee4364d8c24fa728fe493ff54939e16e51f702d
John Meneses <jmmenes90@gmail.com>
2021-02-23 08:37:53 -0500

    Finished.

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-/******************************
- * YOUR CODE GOES RIGHT HERE! *
- ******************************/
+// returns 'Fizz' if the given number is divisible by 3 and not 5
 
+// returns 'Buzz' if the given number is divisible by 5 and not 3
 
+// returns 'FizzBuzz' if the given number is divisible by both 3 and 5
 
+// returns the number if the given number is divisible by neither 3 nor 5
 
-
-
-
-
+function fizzy(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else return num;
+}
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
  * DO NOT ADD OR CHANGE ANYTHING! *
  **********************************/
-
 
 if (typeof fizzy === "undefined") {
   fizzy = undefined;
